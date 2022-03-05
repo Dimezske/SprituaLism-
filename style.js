3a7430c6bca05c2fff2aafd2553e48916ea23b7f
AJ <dimezske@gmail.com>
2022-03-05 16:37:49 +1100

    Add files via upload
    Fixed some scaling problems, and added additional pages, Reiki, Nordic Runes and addition header navigation links

---
+++ style.js
@@ -0,0 +1,10 @@
+/*$(function() {
+    // this initializes the dialog (and uses some common options that I do)
+    $("#dialog").dialog({autoOpen : false, modal : true, show : "blind", hide : "blind"});
+  
+    // next add the onclick handler
+    $("#contactUs").click(function() {
+      $("#dialog").dialog("open");
+      return false;
+    });
+  });*/
